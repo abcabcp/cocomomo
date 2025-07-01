fdc4362a47b64c8ce07aaa6a7f5bc93000dfa723
seulgi.lee <dltmfrl600@gmail.com>
2025-07-01 18:46:19 +0900

    Feat: timePanel reset

--- widgets/layout/TimePanel.tsx
+++ widgets/layout/TimePanel.tsx
@@ -2,36 +2,36 @@
 
 import { formatTimeString, SlidePanel, SnapScrollPicker } from "@/shared";
 import { useTimeControl } from "@/shared/lib/hooks/useTimeControl";
+import Image from "next/image";
 
 export function TimePanel() {
-    const { currentTime, selectedTime, handleTimeChange, isOpen, setIsOpen, handleClose } = useTimeControl();
+    const { isOpen, isReset, currentTime, selectedTime, handleTimeChange, setIsOpen, handleClose, resetTime } = useTimeControl();
 
     return (
         <>
             <button onClick={() => setIsOpen(prev => !prev)} className="hover:opacity-80">
                 {formatTimeString(currentTime.hour, currentTime.minute, currentTime.period)}
             </button>
-
             <SlidePanel
                 isOpen={isOpen}
                 onClose={handleClose}
                 direction="right"
-                wrapperClassName="mt-6 bg-transparent¡¡"
-                className="mt-6 h-fit rounded-2xl p-4"
+                wrapperClassName="mt-6 bg-transparent"
+                className="mt-6 h-fit rounded-2xl p-4 bg-white/40"
                 marginX={16}
             >
-                <>
-                    <h3 className="text-lg font-medium mb-4">Time Picker</h3>
-                    <SnapScrollPicker
-                        type="time"
-                        value={formatTimeString(
-                            selectedTime.hour,
-                            selectedTime.minute,
-                            selectedTime.period as 'AM' | 'PM'
-                        )}
-                        onChange={handleTimeChange}
-                    />
-                </>
+                <button onClick={resetTime} className="flex items-center justify-end">
+                    <Image src="/assets/svgs/reset.svg" alt="reset" width={24} height={24} className={isReset ? "animate-spin" : ""} />
+                </button>
+                <SnapScrollPicker
+                    type="time"
+                    value={formatTimeString(
+                        selectedTime.hour,
+                        selectedTime.minute,
+                        selectedTime.period as 'AM' | 'PM'
+                    )}
+                    onChange={handleTimeChange}
+                />
             </SlidePanel>
         </>
     );
