e74d088d63e06c5c146a20a646d0fcfa6d25eaf8
seulgi.lee <dltmfrl600@gmail.com>
2025-07-03 18:28:12 +0900

    Feat: blog 뼈대 작업

--- features/blog/ui/Blog.tsx
+++ features/blog/ui/Blog.tsx
@@ -1,7 +1,21 @@
+'use client';
+
+import { useRef, useState } from "react";
+import { BlogSidebar } from "./BlogSidebar";
+import { Post } from "./Post";
+
 export function Blog() {
+    const wrapperRef = useRef<HTMLDivElement>(null);
+    const [asidePanelWidth, setAsidePanelWidth] = useState(200);
+
     return (
-        <div className="text-2xl font-bold p-5">
-            Blog!
+        <div ref={wrapperRef} className="w-full h-full flex border-gray-400 bg-gray-900">
+            <BlogSidebar
+                asidePanelWidth={asidePanelWidth}
+                setAsidePanelWidth={setAsidePanelWidth}
+                wrapperRef={wrapperRef}
+            />
+            <Post asidePanelWidth={asidePanelWidth} />
         </div>
     );
 }
