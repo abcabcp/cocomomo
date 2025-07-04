66eb45926ce197362f257b9bd2ca19cb504f8638
seulgi.lee <dltmfrl600@gmail.com>
2025-07-04 16:26:26 +0900

    Feat: Header Submenu

--- widgets/layout/Header.tsx
+++ widgets/layout/Header.tsx
@@ -1,23 +1,38 @@
 'use client';
 
-import Link from 'next/link';
+import { cn, isMobileDevice } from '@/shared';
 import Image from 'next/image';
 import { usePathname } from 'next/navigation';
-import { cn, isMobileDevice } from '@/shared';
-import { TimePanel } from './TimePanel';
+import { useState } from 'react';
 import { SearchBar } from './SearchBar';
+import { SubMenu } from './SubMenu';
+import { TimePanel } from './TimePanel';
 
 type HeaderProps = {
     visible: boolean;
 };
 
+export enum SUBMENU_STATE {
+    NONE = -1,
+    COCOMOMO = 0,
+    HELP = 1,
+}
+
 export function Header({ visible }: HeaderProps) {
+    const [dropdownState, setDropdownState] = useState<SUBMENU_STATE>(SUBMENU_STATE.NONE);
     const pathname = usePathname();
+
+    const handleDropdownClick = (target: SUBMENU_STATE) => {
+        if (dropdownState === target) {
+            return;
+        }
+        setDropdownState(target)
+    };
 
     return (
         <header
             className={cn(
-                "fixed top-0 left-0 right-0 w-full bg-white/80 flex justify-between items-center text-sm px-4 z-30",
+                "fixed top-0 left-0 right-0 w-full bg-black flex justify-between items-center text-sm font-bold px-4 z-30",
                 "transition-transform duration-300 ease-in-out",
                 isMobileDevice()
                     ? (!visible ? "transform -translate-y-full" : "transform translate-y-0")
@@ -25,16 +40,35 @@ export function Header({ visible }: HeaderProps) {
                 pathname === '/' && "lg:transform-none"
             )}
         >
-            <div className='flex gap-x-2 items-center text-black'>
-                <h1 className='cursor-pointer'>
-                    <Link href="/">
-                        <Image src="/assets/svgs/logo.svg" alt="logo" width={24} height={24} />
-                        <span className='sr-only'>COCOMOMO</span>
-                    </Link>
-                </h1>
-                <ul className='flex gap-x-2'>
-                    <li><button>도움말</button></li>
-                </ul>
+            <div className='flex gap-x-2 items-center text-gray-100'>
+                <button
+                    className='cursor-pointer'
+                    onClick={() => handleDropdownClick(SUBMENU_STATE.COCOMOMO)}
+                    onMouseEnter={() => {
+                        if (dropdownState === SUBMENU_STATE.HELP) {
+                            setDropdownState(SUBMENU_STATE.COCOMOMO)
+                        }
+                    }}
+                >
+                    <Image src="/assets/svgs/logo.svg" alt="logo" width={24} height={24} />
+                    <h1 className='sr-only'>COCOMOMO</h1>
+                </button>
+                <button
+                    className='cursor-pointer'
+                    onClick={() => handleDropdownClick(SUBMENU_STATE.HELP)}
+                    onMouseEnter={() => {
+                        if (dropdownState === SUBMENU_STATE.COCOMOMO) {
+                            setDropdownState(SUBMENU_STATE.HELP)
+                        }
+                    }}
+                >
+                    도움말
+                </button>
+                {dropdownState !== SUBMENU_STATE.NONE &&
+                    <SubMenu
+                        dropdownState={dropdownState}
+                        handleDropdownClick={handleDropdownClick}
+                    />}
             </div>
             <ul className='flex gap-x-2 text-black items-center'>
                 <li>
@@ -47,6 +81,6 @@ export function Header({ visible }: HeaderProps) {
                     <TimePanel />
                 </li>
             </ul>
-        </header>
+        </header >
     );
 }
